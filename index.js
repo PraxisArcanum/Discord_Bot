b6ecafa97d96f39e7e281f076aa4d0c4920005f0
tristan-orourke <tristan.m.orourke@gmail.com>
2020-04-11 16:17:49 -0400

    Fix some bugs in force command

--- index.js
+++ index.js
@@ -529,35 +529,48 @@ client.on('message', message=>{
                     message.channel.send('Card format was invalid. Please format as !force @player value of suit property input.');
                     return;
                 }
-                forcedcard = mygame.decks[deckid].cards.filter(card => card.value == c_value && card.suit == c_suit && card.owner == playerid); // should only return one card
-                if (forcedcard.length != 1){
-                    message.channel.send('This card was an invalid part of the deck. Either too many or too few of them exist. This is a weird bug.');
+                const forced_card = mygame.decks[deckid].cards.find(card => {
+                    return  card.value.toLowerCase() == c_value.toLowerCase() 
+                        && card.suit.toLowerCase() == c_suit.toLowerCase() 
+                        && card.owner == playerid
+                }); // may return one card or undefined
+                if (forced_card === undefined){
+                    message.channel.send('This card should be valid, but was not found.');
+                    return;
                 }
                 
-                switch (args[5].toLowerCase()){
+                const property = args[5].toLowerCase();
+                const new_value = args[6];
+                switch (property){
                     case 'location':
                         const possible_locations = ['hand','deck','xp','discard','lost','reserve']; //TODO: These locations, along with values and suits, should be constants defined in one common location.
-                        if (possible_locations.includes(args[6].toLowerCase())){
-                            forcedcard[0].location = args[6];
+                        if (possible_locations.includes(new_value.toLowerCase())){
+                            message.channel.send('The ' + forced_card.name() + ' was forced to the ' + new_value);
+                            forced_card.location = new_value;
+                        } else {
+                            message.channel.send(new_value + ' is not a valid location.');
                         }
-                        message.channel.send('The ' + c_value + ' of ' + c_suit + 'was forced to the ' + args[6]);
                         break;
                     case 'praxis':
-                        Deck.create_praxis(forcedcard[0],message, c_value, c_suit);
+                        Deck.create_praxis(forced_card,message, c_value, c_suit);
                         break;
                     case 'value':
                         const possible_values = ['a','2','3','4','5','6','7','8','9'];
-                        if (possible_values.includes(args[6].toLowerCase())){
-                            forcedcard[0].value = args[6];
+                        if (possible_values.includes(new_value.toLowerCase())){
+                            message.channel.send('The ' + forced_card.name() + ' was forced to become a ' + new_value);
+                            forced_card.value = new_value;
+                        } else {
+                            message.channel.send(new_value + ' is not a valid value.');
                         }
-                        message.channel.send('The ' + c_value + ' of ' + c_suit + 'was forced to become a ' + args[6]);
                         break;
                     case 'suit':
                         const possible_suits = ['clubs','spades','diamonds','hearts'];
-                        if (possible_suits.includes(args[6].toLowerCase())){
-                            forcedcard[0].suit = args[6];
+                        if (possible_suits.includes(new_value.toLowerCase())){
+                            forced_card.suit = new_value;
+                            message.channel.send('The ' + forced_card.name() + ' was forced to become a ' + new_value);
+                        } else {
+                            message.channel.send(new_value + ' is not a valid suit.');
                         }
-                        message.channel.send('The ' + c_value + ' of ' + c_suit + 'was forced to become a ' + args[6]);
                         break;
                 }  
                 
