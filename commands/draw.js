0862c02b85bc1d16626f03824976bf87738b9a7d
tristan-orourke <tristan.m.orourke@gmail.com>
2020-04-11 14:24:37 -0400

    Use a draw_cards helper function to simplify draw command.

--- commands/draw.js
+++ commands/draw.js
@@ -1,32 +1,18 @@
 // Stuff to implement later: max hand size
+const Deck = require('../deckHelpers.js');
 
 module.exports = {
     name: 'draw',
     description: 'Draws a card from a deck',
     execute(message, args, deck){
         // Make sure there are cards to draw
-        let somethingtodraw = 0;
-        for (let i = 0; i < deck.cards.length; i++){
-            somethingtodraw += (deck.cards[i].location == 'deck'); //so long as there are cards in the deck...
+        const drawn_cards = Deck.drawn_cards(deck, 1);
+        for(let i=0; i < drawn_cards.length; i++) {
+            const card = drawn_cards[i];
+            // Set the new location of that card to be in hand
+            card.location = 'hand';
+            console.log('Drew the ' + card.name() + ' from deck to hand.');
+            message.channel.send('Drew the ' + card.name());
         }
-        if (somethingtodraw < 1){
-            message.channel.send('Nothing left to draw, you should reshuffle');
-            return;
-        }
-
-        // Get a random card index (0:length) and make sure it's in the deck
-        let indexpulled = Math.floor( Math.random() * (deck.cards.length));
-        while (deck.cards[indexpulled].location != 'deck'){
-            // TODO: This can be made more efficient.
-            indexpulled = Math.floor( Math.random() * (deck.cards.length));
-        }
-        console.log('Drew card number ' + indexpulled);
-        message.channel.send('Drew the '+deck.cards[indexpulled].value + ' of ' + deck.cards[indexpulled].suit);
-
-        // Set the new location of that card to be in hand
-        console.log(deck.cards[indexpulled].location);
-        deck.cards[indexpulled].location = 'hand';
-        console.log(deck.cards[indexpulled].location);
-
     }
 }
